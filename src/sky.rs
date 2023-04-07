914c3eab3aa7acc2d9bcd0b4f2f4f4101948ea70
Edmund Horner <ejrh00@gmail.com>
2023-04-07 18:21:09 +1200

    Add a moving sun

--- src/sky.rs
+++ src/sky.rs
@@ -1,3 +1,4 @@
+use std::f32::consts::PI;
 use bevy::prelude::*;
 
 #[derive(Default)]
@@ -7,14 +8,20 @@ pub(crate) struct SkyPlugin {
 impl Plugin for SkyPlugin {
     fn build(&self, app: &mut App) {
         app
-        .add_startup_system(create_lights);
+        .add_startup_system(create_lights)
+        .add_system(move_sun);
    }
+}
+
+#[derive(Component)]
+struct Sun {
+    angle: f32
 }
 
 fn create_lights(mut commands: Commands) {
     commands.insert_resource(AmbientLight {
         color: Color::WHITE,
-        brightness: 0.1,
+        brightness: 0.02,
     });
 
     commands.spawn(DirectionalLightBundle {
@@ -25,5 +32,26 @@ fn create_lights(mut commands: Commands) {
         },
         transform: Transform::from_xyz(100.0, 100.0, 100.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
-    });
+    }).insert(Sun { angle: 0.0 });
+}
+
+fn move_sun(time: Res<Time>, mut query: Query<(&mut Sun, &mut Transform, &mut DirectionalLight)>) {
+    let (mut sun, mut transform, mut light) = query.single_mut();
+
+    sun.angle += 2.0*PI/100.0 * time.delta_seconds();
+    let where_in_sky = Quat::from_axis_angle(Vec3::Z, sun.angle);
+    let where_in_sky= where_in_sky.mul_vec3(Vec3::X);
+    let altitude = where_in_sky.dot(Vec3::Y);
+
+    if altitude > 0.5 {
+        light.color = Color::WHITE;
+    } else if altitude > 0.1 {
+        light.color = Color::from([1.0, altitude*2.0, altitude*2.0])
+    } else if altitude > 0.0 {
+        light.color = Color::from([altitude*10.0, altitude*2.0, altitude*2.0])
+    } else {
+        light.color = Color::BLACK;
+    }
+    let where_in_sky = where_in_sky * 200.0;
+    *transform = Transform::from_translation(where_in_sky).looking_at(Vec3::ZERO, Vec3::Y);
 }
