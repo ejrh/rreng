ba635455c28087f0691446504db9a080a93bd52e
Edmund Horner <ejrh00@gmail.com>
2024-08-29 18:58:10 +1200

    Use the available space when running in the browser

--- src/bin/rreng.rs
+++ src/bin/rreng.rs
@@ -8,10 +8,23 @@ use rreng::*;
 fn main() {
     let mut app = App::new();
 
+    #[cfg(target_arch = "wasm32")]
+    let window_plugin = WindowPlugin {
+        primary_window: Some(Window {
+            fit_canvas_to_parent: true,
+            ..default()
+        }),
+        ..default()
+    };
+
+    #[cfg(not(target_arch = "wasm32"))]
+    let window_plugin = WindowPlugin::default();
+
     app
         .add_plugins(
             DefaultPlugins
                 .set(AssetPlugin { meta_check: AssetMetaCheck::Never, ..default() })
+                .set(window_plugin)
         ).add_plugins(FrameTimeDiagnosticsPlugin::default())
         .add_plugins(camera::CameraPlugin::default())
         .add_plugins(sky::SkyPlugin::default())
