5eb78fbaa64a74999f6ab0870c0e9fa0b1f8fcb6
Edmund Horner <ejrh00@gmail.com>
2024-12-05 22:25:08 +1300

    Draw a grid of terrain blocks in debug mode

--- src/debug.rs
+++ src/debug.rs
@@ -1,5 +1,7 @@
+use std::f32::consts::PI;
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use crate::terrain::terrain::Terrain;
 
 #[derive(Default)]
 pub struct DebugPlugin;
@@ -25,8 +27,8 @@ impl Plugin for DebugPlugin {
         app
             .init_state::<DebugState>()
             .add_plugins(WorldInspectorPlugin::new().run_if(in_state(DebugState::On)))
+            .add_systems(Update, debug_terrain.run_if(in_state(DebugState::On)))
             .add_systems(Update, toggle_debug);
-
     }
 }
 
@@ -37,5 +39,29 @@ fn toggle_debug(
 ) {
     if keys.just_pressed(KeyCode::F5) {
         next_state.set(current_state.inverse());
+    }
+}
+
+fn debug_terrain(
+    terrain: Res<Terrain>,
+    mut gizmos: Gizmos,
+) {
+    let pos = Vec3::new(0.5 * terrain.size[1] as f32,0.0,  0.5 * terrain.size[0] as f32);
+    let size = Vec2::new(terrain.size[1] as f32, terrain.size[0] as f32);
+    gizmos.rect(pos, Quat::from_axis_angle(Vec3::X, PI/2.0), size, Color::srgb(1.0, 1.0, 0.5));
+
+    let h = terrain.size[0] as f32;
+    let w = terrain.size[1] as f32;
+
+    for i in 1..terrain.num_blocks[0] {
+        let p1 = Vec3::new(0.0, 0.0, (i * terrain.block_size) as f32);
+        let p2 = Vec3::new(w, 0.0, (i * terrain.block_size) as f32);
+        gizmos.line(p1, p2, Color::srgb(0.5, 0.5, 0.25));
+    }
+
+    for i in 1..terrain.num_blocks[1] {
+        let p1 = Vec3::new((i * terrain.block_size) as f32, 0.0, 0.0);
+        let p2 = Vec3::new((i * terrain.block_size) as f32, 0.0, h);
+        gizmos.line(p1, p2, Color::srgb(0.5, 0.5, 0.25));
     }
 }
